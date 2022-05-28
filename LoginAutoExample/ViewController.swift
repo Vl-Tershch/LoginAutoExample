d121f6a3b714818f4ed74ab6bc8d84ce71030d60
Vl-Tershch <ddiavall@yandex.ru>
2022-05-28 23:40:43 +0300

    Initial Commit

---
+++ LoginAutoExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginAutoExample
+//
+//  Created by Vladislav Tereshchenko on 28.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
